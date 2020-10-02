bcca7ebfb2e39ac87327c6da5ea69d4c36a331c7
Desist-Daydream <39847025+Desist-Daydream@users.noreply.github.com>
2020-10-02 22:29:15 +0800

    yaml 解析库

---
+++ yaml/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"log"
+)
+
+//Nginx nginx  配置
+type Nginx struct {
+	Port int `yaml:"Port"`
+	LogPath string `yaml:"LogPath"`
+	Path string `yaml:"Path"`
+}
+//Config   系统配置配置
+type Config struct{
+	Name string `yaml:"SiteName"`
+	Addr string `yaml:"SiteAddr"`
+	HTTPS bool `yaml:"Https"`
+	SiteNginx  Nginx `yaml:"Nginx"`
+}
+
+func main() {
+	var setting Config
+	config, errRead := ioutil.ReadFile("./info.yaml")
+	if errRead != nil {
+		fmt.Print(errRead)
+	}
+	errUnmarshal := yaml.Unmarshal(config,&setting)
+	if errUnmarshal != nil {
+		log.Fatalf("error: %v", errUnmarshal)
+	}
+
+	fmt.Println(setting.Name)
+	fmt.Println(setting.Addr)
+	fmt.Println(setting.HTTPS)
+	fmt.Println(setting.SiteNginx.Port)
+	fmt.Println(setting.SiteNginx.LogPath)
+	fmt.Println(setting.SiteNginx.Path)
+}
