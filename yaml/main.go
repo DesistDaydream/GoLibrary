ed41271927376e5e584fe35c4898b388b046c1bf
DesistDaydream <373406000@qq.com>
2020-10-09 16:37:01 +0800

    Update main.go

--- yaml/main.go
+++ yaml/main.go
@@ -34,7 +34,7 @@ func main() {
 	// 这种方式存储的 yaml 数据，单行 yaml 格式无法显示 key
 	t := T{}
 
-	// Unmarshal 具有打散、解包等含义。用于将参数2的数据进行解码后，传给参数1。
+	// Unmarshal 具有打散、解包等含义。将参数2中的数据进行解码后，传给参数1。
 	// 官方文档称 Unmarshel 的行为 decode(解码) yaml 数据。
 	// 用白话说就是将多行 yaml 格式合并为1行 yaml 格式。
 	err := yaml.Unmarshal([]byte(data), &t)
@@ -44,7 +44,7 @@ func main() {
 	fmt.Printf("--- 单行 yaml 格式:\n%v\n", t)
 	fmt.Printf("注意：使用 Unmarshal 处理后的的数据常用于进行值传递，比如将这种解析后的数据传递到 go 模板、或者传递到 kubeclt 后进行二次加工并输出等等\n\n")
 
-	// Marshel 具有整理、排列、打包、编组等含义。用于给定的值序列化为 YAML 文档格式。
+	// Marshel 具有整理、排列、打包、编组等含义。将参数中的数据进行编码，并返回序列化后的 yaml 数据。
 	// 官方文档称 Marshel 的行为 encode(编码) yaml 数据。
 	// Marshel() 参数可以接受的数据格式为 `(...) yaml:"[<key>][,<flag1>[,<flag2>]]" (...)`
 	d1, err := yaml.Marshal(&t)
