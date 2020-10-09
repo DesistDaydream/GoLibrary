3937cc34ca9de102c6c666932a0d9857d2fe74b8
DesistDaydream <373406000@qq.com>
2020-10-09 11:44:43 +0800

    2020.10.9

--- Standard_Library/template/yamlDataAppliesTmpl/main.go
+++ Standard_Library/template/yamlDataAppliesTmpl/main.go
@@ -23,8 +23,8 @@ type Nginx struct {
 	Path    string `yaml:"path"`
 }
 
-//Info 示例文件信息
-type Info struct {
+//YamlInfo yaml文件信息
+type YamlInfo struct {
 	Name  string `yaml:"name"`
 	User  []User `yaml:"user"`
 	Nginx Nginx  `yaml:"nginx"`
@@ -34,18 +34,18 @@ func main() {
 	// 创建并解析模板文件
 	t := template.Must(template.ParseFiles("template/yaml.tmpl"))
 
-	// 读取 yaml 文件内容，并将内容放入 config 中后，通过 Unmarshal 处理内容后，放入 setting 中。
+	// 读取 yaml 文件内容，并将内容放入 config 中后，通过 Unmarshal 处理内容，再放入 yamlInfo 中。
 	config, errRead := ioutil.ReadFile("./info.yaml")
 	if errRead != nil {
 		fmt.Print(errRead)
 	}
 
-	var setting Info
-	errUnmarshal := yaml.Unmarshal(config, &setting)
+	var yamlInfo YamlInfo
+	errUnmarshal := yaml.Unmarshal(config, &yamlInfo)
 	if errUnmarshal != nil {
 		log.Fatalf("error: %v", errUnmarshal)
 	}
 
 	// 将处理好的所有 yaml 文件中的内容，应用于模板，并输出。
-	t.Execute(os.Stdout, setting)
+	t.Execute(os.Stdout, yamlInfo)
 }
