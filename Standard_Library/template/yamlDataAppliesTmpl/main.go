9bca75b2c74d9bd4159e02385886d50e0af5092e
DesistDaydream <373406000@qq.com>
2020-10-09 16:14:37 +0800

    2020.10.9

--- Standard_Library/template/yamlDataAppliesTmpl/main.go
+++ Standard_Library/template/yamlDataAppliesTmpl/main.go
@@ -10,38 +10,18 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// User 用户信息
-type User struct {
-	Name string `yaml:"name"`
-	Addr string `yaml:"addr"`
-}
-
-//Nginx nginx  配置
-type Nginx struct {
-	Port    int    `yaml:"port"`
-	LogPath string `yaml:"logPath"`
-	Path    string `yaml:"path"`
-}
-
-//YamlInfo yaml文件信息
-type YamlInfo struct {
-	Name  string `yaml:"name"`
-	User  []User `yaml:"user"`
-	Nginx Nginx  `yaml:"nginx"`
-}
-
 func main() {
 	// 创建并解析模板文件
-	t := template.Must(template.ParseFiles("template/yaml.tmpl"))
+	t := template.Must(template.ParseFiles("./template/yaml.tmpl"))
 
 	// 读取 yaml 文件内容，并将内容放入 config 中后
-	config, errRead := ioutil.ReadFile("./info.yaml")
+	config, errRead := ioutil.ReadFile("./template/info.yaml")
 	if errRead != nil {
 		fmt.Print(errRead)
 	}
 
-	// 声明结构体，用于存储 yaml 格式的数据
-	var yamlInfo YamlInfo
+	// make 一个存储 yaml 数据的地方，通过这种方式，不用关联结构体，直接将 yaml 数据传到模板中。
+	yamlInfo := make(map[interface{}]interface{})
 
 	// 通过 Unmarshal 解析 yaml 格式数据，并将解析结果放入 yamlInfo 中。
 	errUnmarshal := yaml.Unmarshal(config, &yamlInfo)
